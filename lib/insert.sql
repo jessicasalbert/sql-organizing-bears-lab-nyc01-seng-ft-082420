94918f8293b381967a492ba1339c1ada17cba8ea
jessicasalbert <jessicarsalbert@gmail.com>
2020-09-01 15:33:15 -0400

    completed lab - last method failing

---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("sally", 10, "F", "black", "angry", true);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("jess", 1, "MF", "black", "angry", true);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("rach", 13, "F", "brown", "angry", true);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("jake", 17, "M", "black", "angry", true);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("gabe", 12, "M", "black", "angry", true);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("seyi", 10, "M", "black", "angry", true);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("caryn", 10, "F", "brown", "angry", true);
+-- INSERT INTO bears (age, gender, color, temperament, alive) VALUES (10, "M", "black", "angry", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+    ("sally", 10, "F", "black", "angry", true),
+    ("jess", 1, "MF", "black", "angry", true),
+    ("rach", 13, "F", "brown", "angry", true),
+    ("jake", 17, "M", "black", "angry", true),
+    ("gabe", 12, "M", "black", "angry", true),
+    ("seyi", 10, "M", "black", "angry", true),
+    ("caryn", 10, "F", "brown", "angry", true),
+    (NULL, 10, "M", "black", "angry", true);
